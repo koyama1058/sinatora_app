59704d8707cb4c1f9ca5929601560d9fc5901eda
koyama <koyama1058tk@gmail.com>
2021-04-27 14:11:25 +0900

    新規登録・詳細・編集・削除機能の追加

--- app.rb
+++ app.rb
@@ -1,13 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
+require 'json'
+require 'securerandom'
 
-get '/' do
-  erb :index
-end
-
-get '/memo' do
-  binding.pry
+get '/memos' do
+  files = Dir.glob("memos/*").sort_by { |file| File.mtime(file) }
+  @memos = files.map { |file| JSON.parse(File.read(file))}
   erb :index
 end
 
@@ -15,10 +14,32 @@ get '/memo/new' do
   erb :new
 end
 
-get '/memo/edit' do
+post '/memo/new' do
+  hash = { "id" => SecureRandom.uuid, "title" => params["title"], "text" => params["text"]}
+  File.open("memos/#{hash["id"]}.json", "w") { |f| f.puts JSON.generate(hash)}
+  redirect '/memos'
+end
+
+get '/memo/edit/:id' do
+  file = Dir.glob("memos/#{params[:id]}.json")
+  @memo = JSON.parse(File.read(file[0]))
   erb :edit
 end
 
-get '/memo/show' do
+patch "/memo/edit/:id" do
+  hash = { "id" => "#{params[:id]}", "title" => params["title"], "text" => params["text"]}
+  File.open("memos/#{hash["id"]}.json", "w") { |f| f.puts JSON.generate(hash)}
+  redirect '/memos'
+end
+
+get '/memo/show/:id' do
+  file = Dir.glob("memos/#{params[:id]}.json")
+  @memo = JSON.parse(File.read(file[0]))
   erb :show
+end
+
+delete '/memo/:id' do
+  file = file = Dir.glob("memos/#{params[:id]}.json")
+  File.delete(file[0])
+  redirect '/memos'
 end
